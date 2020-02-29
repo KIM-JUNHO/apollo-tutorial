a464bf17037eb4708b8c47ea6bfb3553fec1bc9b
Unknown <prpr48@naver.com>
2020-02-29 10:45:15 +0900

    add apolloprovider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,11 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import gql from 'graphql-tag';
+import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import injectStyles from './styles';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -27,3 +32,11 @@ client
     `
   })
   .then(result => console.log(result));
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
