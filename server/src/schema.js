141f1cfc2a1e7cba2a55472771e1a9e51d8f4a8a
Unknown <prpr48@naver.com>
2020-02-29 09:23:22 +0900

    add missinpatch

--- server/src/schema.js
+++ server/src/schema.js
@@ -53,7 +53,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
