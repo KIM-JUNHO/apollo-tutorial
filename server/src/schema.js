cd44f79dcbd5cd9ab56002d4cd74568f3e532c3d
Unknown <prpr48@naver.com>
2020-02-29 09:00:28 +0900

    add array type, argument field

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,28 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
   type Launch {
     id: ID!
     site: String
